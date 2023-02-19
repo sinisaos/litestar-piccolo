a108c2d904a8040537bdadfa87c30f80da4b7a53
sinisaos <sinisaos@gmail.com>
2023-02-19 17:08:36 +0100

    fix task schema

--- backend/tasks/schema.py
+++ backend/tasks/schema.py
@@ -5,7 +5,7 @@ from tasks.tables import Task
 # task models
 TaskModelIn = create_pydantic_model(
     table=Task,
-    exclude_columns=(Task.created_at,),
+    exclude_columns=(Task.created_at, Task.task_user),
     model_name="TaskModelIn",
 )
 TaskModelOut = create_pydantic_model(
