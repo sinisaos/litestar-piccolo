d58964593bf161c00afa1f0d55b472efbac1094e
sinisaos <sinisaos@gmail.com>
2023-02-19 13:29:16 +0100

    add guards to tasks routes

--- backend/tasks/endpoints.py
+++ backend/tasks/endpoints.py
@@ -1,9 +1,11 @@
 import typing as t
 
+from starlite import Request
 from starlite.controller import Controller
 from starlite.exceptions import NotFoundException
 from starlite.handlers import delete, get, patch, post
 
+from accounts.utils import current_user, current_user_guard
 from tasks.schema import TaskModelIn, TaskModelOut
 from tasks.tables import Task
 
@@ -25,27 +27,34 @@ class TaskController(Controller):
         task = await Task.objects().get(Task.id == task_id)
         return task.to_dict()
 
-    @post("/tasks")
-    async def create_task(self, data: TaskModelIn) -> TaskModelOut:
-        task = Task(**data.dict())
+    @post("/tasks", guards=[current_user_guard])
+    async def create_task(
+        self, data: TaskModelIn, request: Request
+    ) -> TaskModelOut:
+        session_user = await current_user(request)
+        data_map = data.dict()
+        data_map["task_user"] = session_user["user"]["id"]
+        task = Task(**data_map)
         await task.save()
         return task.to_dict()
 
-    @patch("/tasks/{task_id:int}")
+    @patch("/tasks/{task_id:int}", guards=[current_user_guard])
     async def update_task(
-        self, task_id: int, data: TaskModelIn
+        self, task_id: int, data: TaskModelIn, request: Request
     ) -> TaskModelOut:
+        session_user = await current_user(request)
         task = await Task.objects().get(Task.id == task_id)
         if not task:
             raise NotFoundException("Task does not exist")
         for key, value in data.dict().items():
             task.id = task_id
+            task.task_user = session_user["user"]["id"]
             setattr(task, key, value)
 
         await task.save()
         return task.to_dict()
 
-    @delete("/tasks/{task_id:int}")
+    @delete("/tasks/{task_id:int}", guards=[current_user_guard])
     async def delete_task(self, task_id: int) -> None:
         task = await Task.objects().get(Task.id == task_id)
         if not task:
