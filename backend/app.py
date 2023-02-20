2202ccf1aeaa141da9537f355f7db33709e069ca
sinisaos <sinisaos@gmail.com>
2023-02-20 07:18:26 +0100

    add CORS middleware

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from piccolo.apps.user.tables import BaseUser
 from piccolo.engine import engine_finder
 from piccolo_admin.endpoints import create_admin
-from starlite import Starlite, asgi
+from starlite import CORSConfig, Starlite, asgi
 
 from accounts.endpoints import AuthController
 from tasks.endpoints import TaskController
@@ -14,6 +14,22 @@ async def admin(
     scope: "Scope", receive: "Receive", send: "Send"  # noqa: F821
 ) -> None:
     await create_admin(tables=[Task, BaseUser])(scope, receive, send)
+
+
+# CORS
+cors_config = CORSConfig(
+    allow_origins=["http://localhost:8080"],
+    allow_methods=[
+        "GET",
+        "POST",
+        "DELETE",
+        "PUT",
+        "PATCH",
+        "OPTIONS",
+    ],
+    allow_headers=["Origin", "Content-Type"],
+    allow_credentials=True,
+)
 
 
 async def open_database_connection_pool():
@@ -38,6 +54,7 @@ app = Starlite(
         AuthController,
         TaskController,
     ],
+    cors_config=cors_config,
     on_startup=[open_database_connection_pool],
     on_shutdown=[close_database_connection_pool],
 )
